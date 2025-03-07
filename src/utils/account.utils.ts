978fa92e274ae0c43e9e748527f108b6ab5e094b
Nik Tsonev <nikolay.l.tsonev@gmail.com>
2025-03-07 20:33:00 +0100

    implemmented the logout and delete function and it should also  be able to remove him locally

--- src/utils/account.utils.ts
+++ src/utils/account.utils.ts
@@ -18,9 +18,10 @@ import { User } from "../hooks/UserContext";
  * @example 
  * 
  * const oncClick = async (user) => {
- *  if (await sendUserData(user, "ENDPOINT") !== 200) { console.log("Saving unsucesfull"); return; }
+ *  const updatedUser = { ...user, displayName: "New Name" };
+ *  if (await sendUserData(updatedUser, "ENDPOINT") !== 200) { console.log("Saving unsucesfull"); return; }
  *  
- *  setUser(user);
+ *  setUser(updatedUser);
  * }
  * 
  * @param {User} user 
@@ -35,6 +36,55 @@ export const sendUserData = async (user: User, endpoint: string): Promise<number
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(user),
         });
+
+        return response.status as number
+
+    } catch (error: any) {
+        console.log(error.message);
+        return 500;
+    }
+};
+
+//TODO: impelement the fetchUser data
+
+/**
+ * when the user logsout something on their account this function should be called first. 
+ * If it is successuful, the function will automatically remove the user from localStorage which
+ * is how variables are stored persitently. Caller of this function must reload the page however 
+ * after use
+ * 
+ * 200 - OK
+ * 
+ * 400 - invallid request
+ * 
+ * 401 - unauthorised
+ * 
+ * 405 - method not allowed
+ * 
+ * 500 - backend issues (exact error in the body of the response)
+ * 
+ * @example 
+ * 
+ * const oncClick = async (endpoint: string) => {
+ *  if (await logout(endpoint) !== 200) { console.log("Saving unsucesfull"); return; }
+ *  
+ *  //RELOAD PAGE
+ * }
+ * 
+ *
+ * 
+ * @param {string} endpoint Chose LOGOUT to logout or DELETE
+ * @returns {number} response code 
+ */
+export const logout = async (endpoint: string): Promise<number> => {
+    const URL: string = `URL_FOR_API/${endpoint}`;
+    try {
+        const response = await fetch(URL, {
+            method: "POST",
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        localStorage.removeItem("user");
 
         return response.status as number
 
