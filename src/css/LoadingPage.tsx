aa84154a00362b6db7226096b6fab79a47509073
Sadat Dauti <sadatdauti@Sadats-MacBook-Air.fritz.box>
2025-03-29 21:41:06 +0100

    Added loading page

---
+++ src/css/LoadingPage.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { Box, Typography } from '@mui/material';
+import ThemeToggleButton from '../Components/ThemeToggleButton';
+import './LoadingPage.css'; // If you have custom styles for the page
+
+
+const LoadingPage: React.FC = () => {
+  return (
+    <Box
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      height="100vh"
+      flexDirection="column"
+    >
+      <Typography variant="h6" mb={2}>
+        Loading... Please Wait
+      </Typography>
+
+      {/* Your hamster animation */}
+      <div
+        aria-label="Orange and tan hamster running in a metal wheel"
+        role="img"
+        className="wheel-and-hamster"
+      >
+        <div className="wheel"></div>
+        <div className="hamster">
+          <div className="hamster__body">
+            <div className="hamster__head">
+              <div className="hamster__ear"></div>
+              <div className="hamster__eye"></div>
+              <div className="hamster__nose"></div>
+            </div>
+            <div className="hamster__limb hamster__limb--fr"></div>
+            <div className="hamster__limb hamster__limb--fl"></div>
+            <div className="hamster__limb hamster__limb--br"></div>
+            <div className="hamster__limb hamster__limb--bl"></div>
+            <div className="hamster__tail"></div>
+          </div>
+        </div>
+        <div className="spoke"></div>
+      </div>
+
+      {/* Include the ThemeToggleButton here */}
+      <ThemeToggleButton />
+    </Box>
+  );
+};
+
+export default LoadingPage;
+
+
+
+
+
+
+
+
+
