a4a11ae7416a6737f76b7ba7894769801432d294
akamran <a.kamran1128@gmail.com>
2025-05-08 23:51:16 +0200

    123

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -8,7 +8,7 @@ export type Project = {
   members?: ProjectMember[];
   issues?: Issue[];
   tags?: Tag[];
-}
+};
 
 export type Organization = {
   id?: number;
@@ -16,7 +16,7 @@ export type Organization = {
   projects?: Project[];
   members?: OrgMember[];
   roles?: OrgRole[];
-}
+};
 
 export type User = {
   displayName: string;
@@ -31,7 +31,7 @@ export type User = {
   projects?: ProjectMember[];
   issues?: Issue[];
   sessions?: Session[];
-}
+};
 
 export type Issue = {
   id: number;
@@ -44,14 +44,14 @@ export type Issue = {
   cost: number;
   dependencies?: Dependency[];
   reminders?: Reminder[];
-}
+};
 
 export type OrgMember = {
   id: number;
   userId: number;
   orgId: number;
   roles?: OrgRole[];
-}
+};
 
 export type OrgRole = {
   id: number;
@@ -60,14 +60,14 @@ export type OrgRole = {
   canRead: boolean;
   canWrite: boolean;
   canExec: boolean;
-}
+};
 
 export type ProjectMember = {
   id: number;
   userId: number;
   projectId: number;
   roles?: ProjectRole[];
-}
+};
 
 export type ProjectRole = {
   id: number;
@@ -76,187 +76,69 @@ export type ProjectRole = {
   canRead: boolean;
   canWrite: boolean;
   canExec: boolean;
-}
+};
 
 export type Tag = {
   id: number;
   projectId: number;
   name: string;
   color: string;
-}
+};
 
 export type Dependency = {
   id: number;
   issueId: number;
   dependency: number;
-}
+};
 
 export type Reminder = {
   id: number;
   issueId: number;
   userId: number;
-}
+};
 
 export type Session = {
   id: number;
   userId: number;
   expires: Date;
-}
+};
 
 export type VerifyUser = {
   id: number;
   code: number;
   expires: Date;
-}
+};
 
 export type ForgotPassword = {
   id: number;
   userId: number;
   code: number;
   expirationDate: Date;
+};
+
+export interface Board {
+  id: number;
+  name: string;
+  createdBy: string;
+  createdById: string;
+  createdAt: Date;
+  tasks: Task[];
 }
-=======
-    id: number;
-    name: string;
-    orgId: number;
-    budget: number;
-    charter: string;
-    archived: boolean;
-    members?: ProjectMember[];
-    issues?: Issue[];
-    tags?: Tag[];
-  }
-  
-  export type Organization = {
-    id?: number;
-    name: string;
-    projects?: Project[];
-    members?: OrgMember[];
-    roles?: OrgRole[];
-  }
-  
-  export type User = {
-    displayName: string;
-    id: number;
-    email: string;
-    name: string;
-    password: string;
-    avatar?: string | null;
-    verified: boolean;
-    verifyId?: number | null;
-    organizations?: OrgMember[];
-    projects?: ProjectMember[];
-    issues?: Issue[];
-    sessions?: Session[];
-  }
-  
-  export type Issue = {
-    id: number;
-    title: string;
-    desc?: string | null;
-    tagId?: number | null;
-    priority?: number | null;
-    created: Date;
-    completed?: Date | null;
-    cost: number;
-    dependencies?: Dependency[];
-    reminders?: Reminder[];
-  }
-  
-  export type OrgMember = {
-    id: number;
-    userId: number;
-    orgId: number;
-    roles?: OrgRole[];
-  }
-  
-  export type OrgRole = {
-    id: number;
-    orgId: number;
-    name: string;
-    canRead: boolean;
-    canWrite: boolean;
-    canExec: boolean;
-  }
-  
-  export type ProjectMember = {
-    id: number;
-    userId: number;
-    projectId: number;
-    roles?: ProjectRole[];
-  }
-  
-  export type ProjectRole = {
-    id: number;
-    projectId: number;
-    name: string;
-    canRead: boolean;
-    canWrite: boolean;
-    canExec: boolean;
-  }
-  
-  export type Tag = {
-    id: number;
-    projectId: number;
-    name: string;
-    color: string;
-  }
-  
-  export type Dependency = {
-    id: number;
-    issueId: number;
-    dependency: number;
-  }
-  
-  export type Reminder = {
-    id: number;
-    issueId: number;
-    userId: number;
-  }
-  
-  export type Session = {
-    id: number;
-    userId: number;
-    expires: Date;
-  }
-  
-  export type VerifyUser = {
-    id: number;
-    code: number;
-    expires: Date;
-  }
-  
-  export type ForgotPassword = {
-    id: number;
-    userId: number;
-    code: number;
-    expirationDate: Date;
-  }
-  
-  export interface Board {
-    id: number;
-    name: string;
-    createdBy: string;
-    createdById: string;
-    createdAt: Date;
-    tasks: Task[];
-  }
-  
-  export interface Task {
-    id: string;
-    title: string;
-    desc: string;
-    tagid: number;
-    priority: number;
-    cost: number;
-    created: Date;
-    createdBy: string;
-    completed?: Date;
-  }
-  
-  
-  export interface NewBoard {
-    name: string;
-    createdBy: string;
-    createdAt: Date;
-  }
+
+export interface Task {
+  id: string;
+  title: string;
+  desc: string;
+  tagid: number;
+  priority: number;
+  cost: number;
+  created: Date;
+  createdBy: string;
+  completed?: Date;
+}
+
+export interface NewBoard {
+  name: string;
+  createdBy: string;
+  createdAt: Date;
+}
